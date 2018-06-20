100f18954a3cf86d5f74229c66f0ded8baf5665b
veshitala <veshitala@gmail.com>
2018-06-20 22:15:57 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+  $('.navbar-toggler').click(function () {
+    $('.navbar-toggler').toggleClass('change');
+
+  })
+
+  //window scroll
+
+  $(window).scroll(function () {
+    let position = $(this).scrollTop();
+
+    if (position >= 149) {
+      $('.navbar').addClass('navbar-background')
+      $('.navbar').addClass('fixed-top');
+    } else {
+      $('.navbar').removeClass('navbar-background');
+      $('.navbar').removeClass('fixed-top');
+    }
+  })
+  //smooth scroll
+  $('.nav-item a').click(function (link) {
+    link.preventDefault();
+    let target = $(this).attr('href');
+    $('html, body').stop().animate({
+      scrollTop: $(target).offset().top
+    }, 2000);
+
+  })
+//banner scroll
+
+  $('.banner-btn').click(function (link) {
+    link.preventDefault();
+    let target = $(this).attr('href');
+    $('html, body').stop().animate({
+      scrollTop: $(target).offset().top
+    }, 3000);
+
+  })
+
+  // Isotope
+  let $grid = $('.grid').isotope({
+    // options
+  });
+  // filter items on button click
+  $('.filter-button-group').on('click', 'button', function () {
+    let filterValue = $(this).attr('data-filter');
+    $grid.isotope({
+      filter: filterValue
+    });
+  });
+
+})
